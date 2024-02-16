b8e326a69783dabcfc087142573ebc644a69a89d
Santiago Finamore <18-10125@usb.ve>
2024-02-15 20:44:35 -0400

    pregunta_2. LO HICIMOS EN TIEMPO O(N^2) LESSGOOOOOOOOOO

--- pregunta_2/src/main.rs
+++ pregunta_2/src/main.rs
@@ -1,18 +1,20 @@
 fn good_subarrays(a: Vec<i32>) -> u32 {
     let mut prev: Vec<u32> = vec![1; a.len()];
-    let mut accum: u32 = a.len() as u32;
+    let mut res: u32 = a.len() as u32;
 
     for len in 2..(a.len() + 1) {
         let mut curr: Vec<u32> = vec![0; a.len()];
+        let mut accum: u32 = 0;
         for index in (len-1)..a.len() {
-            if a[index] % ((len) as i32) == 0 {
-                curr[index] += prev[0..index].iter().sum::<u32>(); //tecnicamente O(n^3) matenme plz
-                accum += curr[index];
+            accum += prev[index-1];
+            if a[index] % (len as i32) == 0 {
+                curr[index] += accum;
+                res += curr[index];
             }
         }
         prev = curr;
     }
-    accum
+    res
  
 }
 
